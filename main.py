af9eba3df3a08828da6a157bf413657e457d6139
bigrael123 <bigrael-x@hotmail.com>
2024-06-21 23:41:41 -0300

    added the original price of the product

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
 product_value = float(input("What is the price of the product?"))
 product_discount = float(input("What is the discount of the product?"))
 product_discount_value = (product_value -  product_value * (product_discount/100))
-print(f" The discount is {product_discount} and the new price of the product is {product_discount_value}")
+print(f"The original price of the product is {product_value} The discount is {product_discount} and the new price of the product is {product_discount_value}")
